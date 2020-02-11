ec4274bbd0bb2d7346030c15d057a60fe1d4830b
Víctor Peña Romero <vic.pero@icloud.com>
2020-02-10 21:27:50 -0600

    Add signup styles

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -12,3 +12,38 @@ const showMenu = () => {
       $bars.style.display = 'flex'
   }
 }
+
+const register = () => {
+
+  const $birthdayLabel = document.querySelector('label[for="birthday"]')
+  const $birthdayInput = document.querySelector('input[name="birthday"]')
+
+  const $validationButton = document.querySelector('#validation')
+
+  const $emailLabel = document.querySelector('label[for="email"]')
+  const $emailInput = document.querySelector('input[name="email"]')
+  const $usernameLabel = document.querySelector('label[for="username"]')
+  const $usernameInput = document.querySelector('input[name="username"]')
+  const $passwordLabel = document.querySelector('label[for="password"]')
+  const $passwordInput = document.querySelector('input[name="password"]')
+  const $submitInput = document.querySelector('input[type="submit"]')
+
+
+
+  if($birthdayInput.value != "") {
+
+    $birthdayLabel.style.display = "none"
+    $birthdayInput.style.display = "none"
+
+    $validationButton.style.display = "none"
+  
+    $emailLabel.style.display = "flex"
+    $emailInput.style.display = "flex"
+    $usernameLabel.style.display = "flex"
+    $usernameInput.style.display = "flex"
+    $passwordLabel.style.display = "flex"
+    $passwordInput.style.display = "flex"
+    $submitInput.style.display = "flex"
+
+  }
+}
