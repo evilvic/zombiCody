a51b54161e06ae1c2d0736bd0f3d4584a7382952
Víctor Peña Romero <vic.pero@icloud.com>
2020-02-11 19:58:34 -0600

    Add create questions functionality

--- controllers/createControllers.js
+++ controllers/createControllers.js
@@ -1,4 +1,5 @@
 const Course = require('../models/Course')
+const Question = require('../models/Question')
 
 exports.createCourseView = (req, res) => {
   res.render('create/course')
@@ -7,18 +8,65 @@ exports.createCourseView = (req, res) => {
 exports.createCoursePost = async (req, res) => {
 
   const { title, description } = req.body
-  console.log(req.body)
-  console.log(req.file)
-  
 
   const { secure_url: image_URL } = req.file
 
-  await Course.create({
+  const newCourse = await Course.create({
     title, 
     description,
     image_URL
   })
 
-  res.redirect('/')
+  res.redirect(`/create/course/${newCourse._id}`)
+
+}
+
+exports.createQuestionView = async (req, res) => {
+  const { id } = req.params
+  const course = await Course.findById(id)
+  res.render('create/question', course)
+}
+
+exports.createQuestionPost = async (req, res) => {
+
+  const { id } = req.params
+
+  const {
+    content,
+    option1,
+    option2,
+    option3,
+    option4,
+    option5,
+    solution,
+    feedback
+  } = req.body
+
+  const { secure_url: image_URL } = req.file
+
+  const newQuestion = await Question.create(
+    {
+      content,
+      image_URL,
+      option1,
+      option2,
+      option3,
+      option4,
+      option5,
+      solution,
+      feedback
+    }
+  )
+
+  const questionId = newQuestion._id
+
+  const course = await Course.findById(id)
+
+  course.questions.push(questionId)
+
+  course.save()
+
+  res.redirect(`/create/course/${id}`)
+
 
 }
--- routes/createRoutes.js
+++ routes/createRoutes.js
@@ -3,11 +3,15 @@ const router = Router()
 const uploadCloud = require('../config/cloudinary')
 const {
   createCourseView,
-  createCoursePost
+  createCoursePost,
+  createQuestionView,
+  createQuestionPost
 } = require('../controllers/createControllers')
 
 router
   .get('/course', createCourseView)
   .post('/course', uploadCloud.single('portrait'), createCoursePost)
+  .get('/course/:id', createQuestionView)
+  .post('/course/:id', uploadCloud.single('image'), createQuestionPost)
 
 module.exports = router
